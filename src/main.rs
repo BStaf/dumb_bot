f66d7ca4dcca01c77a886dd90930d6478978554c
BillS <will.staf@gmail.com>
2024-08-29 23:22:07 -0400

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod reddit_creds;
 
-use reqwest::{Error, Response};
+use reqwest::{Error, Response, StatusCode};
 use reqwest::header::USER_AGENT;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct AccessTokenResponse {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: i32,
+    pub scope: String,
+}
 
 fn get_form_data<'a>(username :&'a str, password :&'a str) -> [(&'a str, &'a str); 3]{
     [
@@ -11,35 +21,48 @@ fn get_form_data<'a>(username :&'a str, password :&'a str) -> [(&'a str, &'a str
     ]
 }
 
-async fn try_reddit(access_token_url: &str, api_creds: &reddit_creds::RedditApiCreds) -> Result<(), reqwest::Error> {
+async fn try_reddit(access_token_url: &str, api_creds: &reddit_creds::RedditApiCreds) -> Result<String, String> {
     
     let form_data = get_form_data(
         &api_creds.username, 
         &api_creds.password);
 
     let client = reqwest::Client::new();
+
     let request = client.post(access_token_url)
         .basic_auth(&api_creds.client_id, Some(&api_creds.client_secret))
         .header("Content-Type", String::from("application/json"))
         .header(USER_AGENT, String::from("curl/7.88.1"))
         .form(&form_data);
 
-    let response = request.send()
-        .await?;
-    println!("Status: {}", response.status());
-    //println!("{:?}", response.);
+    let response = match request.send().await {
+        Ok(r) => r,
+        Err(e) => return Err(e.to_string())
+    };
 
-    Ok(())
+    match response.status() {
+        StatusCode::OK => {
+            let json = match response.json::<AccessTokenResponse>().await {
+                Ok(t) => t,
+                Err(e) => return Err(e.to_string())
+            };
+            Ok(json.access_token)
+        },
+        status_code => Err(format!("Retuned Status Code: {status_code}"))
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), String> {
     let access_token_url = "https://www.reddit.com/api/v1/access_token";
 
     let reddit_creds = reddit_creds::get_creds_from_json(
         reddit_creds::get_creds_file("creds.json")
     ).expect("failed to get creds");
 
-    try_reddit(&access_token_url, &reddit_creds).await?;
+    let token = try_reddit(&access_token_url, &reddit_creds).await?;
+
+    println!("Status: {}", token);
+
     Ok(())
 }
